1dbba31ddbccc9c3bb8148a34903a6b2848358cc
Clay <cerebralnomad@protonmail.com>
2024-05-09 04:07:52 -0400

    updated to 24.04

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Script to install the regular suite of programs on a fresh OS installation
+# Script to install my regular suite of programs on a fresh OS installation
 # Run with sudo
+# This script is for distros other than Kubuntu or other Plasma based DEs
+# The only difference is here Latte Dock, Yakuake, and the Kubuntu backports PPA are not installed
+# Plasma rules Gnome drools
 
 # Check if the script was run as root
 
@@ -12,19 +15,18 @@ fi
 
 clear
 
-# Collect username for error log
-
-echo 'What is your username?'
-echo -n '>>> '
-read USERNAME
-
 # Create the log file for any errors
+
+USERNAME="$SUDO_USER"
 
 sudo -H -u $USERNAME touch /home/$USERNAME/Documents/post_install_error.log 
 
 echo 'Error log created at:'
 echo "/home/$USERNAME/Documents/post_install_error.log"
-read -n 1 -r -s -p "Press any key to continue..."
+sleep 3
+
+# Create the variables for the various packages to be installed
+# Comment out any you do not want/need
 
 # Command line based tools and non-interactive libraries
 
@@ -33,26 +35,23 @@ packages=(
 	curl
 	python3-pip # PIP package manager for python 3
 	python3-venv # Python 3 virtual environment manager
-	default-jdk # Java developer Kit needed for running Eclipse IDE
+	default-jdk # Java developer Kit
+    default-jre # Java Runtime Environment
 	git
 	apt-transport-https # Needed for Syncthing
 	zsh
-	libdvd-pkg
-	dvdbackup
 	)
 
 # GUI applications 
 
 apps=(
 	zim # Personal Wiki 
-	sigil # Edit Epub meta data
 	keepass2 # Local password vault
 	timeshift # system state automatic backup
-	qbittorrent # Torrent downloader
+	qbittorrent # Torrent downloader - will install from 3rd party PPA
 	bleachbit # System cleaner
-	handbrake-gtk # Video encoder
-	mkvtoolnix-gui # Video Muxer
-	syncthing # File Syncronization between systems
+	mkvtoolnix-gui # Video Muxer - will install from 3rd party PPA
+	syncthing # File Syncronization between systems - will install from 3rd party PPA
 	gpa # Graphical frontend for gnupg
 	gnupg2 # PGP handling
 	gnubg  # Backgammon game
@@ -61,39 +60,53 @@ apps=(
 	exiftool  # tool for displaying metadata of epub and other formats
 	bucklespring # utility to make kestrokes mimic IBM keyboard
     nomacs # Image viewer - requires 'Universe' repo to be enabled
+    mediainfo-gui # graphical frontend for mediainfo
     )
 
 # Snap packages
 
-snaps=(
-	gimp
-	gnome-sudoku
-	sublime-text
-	cheat
-	solitaire
-    newpass
-    bashtop
-	)
+#snaps=(
+#    newpass
+#	)
+
+# Flatpaks
+
+flatpaks=(
+        com.discordapp.Discord
+        org.flatpak.Builder
+        fr.handbrake.ghb
+        com.cerebralnomad.recipescribe
+        org.gnome.Sudoku
+        com.sigil_ebook.Sigil
+)
 
 #Terminal addons
 	
 addons=(
-	tree
-	ncdu
-	inxi
-	neofetch
-	htop
-	vim
-	mediainfo
-	nodejs
-	npm
-	fortune
-	cowsay
-	hwinfo
-	terminator
-	dstat
-	hollywood
-	figlet
+	tree # list contents of directories in a tree-like format
+	gcp  # advanced command-line file copier
+	bat  # a cat clone with syntax highlighting
+	ncdu # NCurses Disk Usage utility
+	inxi # Command line system information script
+	fastfetch # neofetch alternative
+    btop  # Resource monitor
+    ffmpeg  # command line video converter
+	vim  # terminal text editor
+	mediainfo # display information about audio/video files
+	nodejs  # JavaScript runtime
+	npm  # JavaScript package manager
+	fortune # display fortunes in the terminal
+    fortunes-off # adds offensive fortunes (fortune -o)
+    fortune-ng 
+    fortune-mod
+    fortune-docs
+    fortune-libs
+	cowsay 
+	hwinfo # display details about system hardware
+	terminator # terminal emulator
+	dstat  # tool for generating system resource statistics 
+	hollywood # silly program to make teh terminal look like 1337 H4X0R
+	figlet # create ASCII art from plain text
 	)
 
 echo "Additional Program Installation for a Fresh OS Install"
@@ -102,60 +115,10 @@ echo ""
 echo "Adding Required Repositories"
 echo "============================"
 echo ""
-# Add the Timeshift repo
-apt-add-repository -y ppa:teejee2008/ppa 2>> /home/$USERNAME/Documents/post_install_error.log && printf '\nTimeshift repo added...\n\n'
 
 # Add the qbittorrent repo
 add-apt-repository -y ppa:qbittorrent-team/qbittorrent-stable 2>> /home/$USERNAME/Documents/post_install_error.log && printf '\nqbittorrent repo added...\n\n'
-
-# Add the Handbrake repo
-add-apt-repository -y ppa:stebbins/handbrake-releases 2>> /home/$USERNAME/Documents/post_install_error.log && printf '\nHandbrake repo added...\n\n'
-
-echo -n "Is this a KDE installation? (y/n): "
-read DISTRO
-if [[ $DISTRO == "y"* || $DISTRO == "Y"* ]] ; then
-	add-apt-repository -y ppa:rikmills/latte-dock 2>> /home/$USERNAME/Documents/post_install_error.log && printf "\nLattee Dock repo added... \n\n"
-	add-apt-repository ppa:kubuntu-ppa/backports 2>> /home/$USERNAME/Documents/post_install_error.log -y && printf "\nKDE backports repo added... \n\n"
-fi
-
-echo "Finished addding repos..."
-echo ""
-echo "Updating Repos and Upgrading System Files"
-echo "========================================="
-apt update && apt upgrade -y
-
-clear
-
-echo "Installing Additional Tools"
-echo "==========================="
-echo ""
-
-# Install the packages first as wget and curl are needed in the next step
-
-for pkg in "${packages[@]}" ; do
-	if apt install -y $pkg ; then 
-		printf "\n$pkg installed... \n\n" 
-	else 
-		printf "\n$pkg install FAILED! \n\n" 2>> /home/$USERNAME/Documents/post_install_error.log
-	fi
-done
-echo ""
-
-# Configure the libdvd package for automatic updates
-
-echo "Time to configure the libdvd package"
-echo "===================================="
-dpkg-reconfigure libdvd-pkg
-
-echo ""
-echo "Adding Additional Sources with wget and curl"
-echo "============================================"
-echo ""
-# Install Oh-My-ZSH
-sudo -H -u $USERNAME sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
-
-# Install Calibre
-wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sh /dev/stdin 2>> /home/$USERNAME/Documents/post_install_error.log
+sleep 2
 
 # MKVToolnix repo
 # There are new repos for every version of Ubuntu
@@ -163,39 +126,94 @@ wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sh /dev/std
 # versions and remove versions no longer supported refer to
 # https://mkvtoolnix.download/downloads.html#ubuntu
 # This should always be set for the current LTS
+# Curently set to 24.04
 
-wget -O /usr/share/keyrings/gpg-pub-moritzbunkus.gpg https://mkvtoolnix.download/gpg-pub-moritzbunkus.gpg 2>> /home/$USERNAME/Documents/post_install_error.log
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/gpg-pub-moritzbunkus.gpg] https://mkvtoolnix.download/ubuntu/ focal main' | tee /etc/apt/sources.list.d/mkvtoolnix.download.list 2>> /home/$USERNAME/Documents/post_install_error.log && printf '\nMKVToolNix repo installed for 20.04\n\n' 
+sudo wget -O /usr/share/keyrings/gpg-pub-moritzbunkus.gpg https://mkvtoolnix.download/gpg-pub-moritzbunkus.gpg 2>> /home/$USERNAME/Documents/post_install_error.log
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/gpg-pub-moritzbunkus.gpg] https://mkvtoolnix.download/ubuntu/ noble main' | tee /etc/apt/sources.list.d/mkvtoolnix.download.list 2>> /home/$USERNAME/Documents/post_install_error.log && printf '\nMKVToolNix repo installed for 24.04\n\n'
 echo ''
+sleep 2
 
 # Syncthing repo
 curl -s https://syncthing.net/release-key.txt | apt-key add - 2>> /home/$USERNAME/Documents/post_install_error.log
 echo 'deb https://apt.syncthing.net/ syncthing stable' | tee /etc/apt/sources.list.d/syncthing.list 2>> /home/$USERNAME/Documents/post_install_error.log && printf '\nSyncthing repo installed\n\n'
+sleep 2
+
+echo "Finished addding repos..."
+echo ""
+echo "Updating Repos and Upgrading System Files"
+echo "========================================="
+apt update && apt dist-upgrade -y
+
+sleep 2
+
+echo "Installing Additional Tools"
+echo "==========================="
+echo ""
+
+# Install the packages first as wget and curl are needed in the next steps
+
+for pkg in "${packages[@]}" ; do
+	if apt install -y $pkg ; then 
+		printf "\n$pkg installed... \n\n"
+        sleep 1 
+	else 
+		printf "\n$pkg install FAILED! \n\n" 2>> /home/$USERNAME/Documents/post_install_error.log
+        sleep 1
+	fi
+done
+echo ""
+sleep 2
+# Install Flatpak because Canonical is being a dick about it
+
+echo "Installing Flatpak"
+echo "=================="
+echo ""
+sudo apt install flatpak
+echo ""
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+echo ""
+sleep 2
+echo "Adding Additional Sources with wget and curl"
+echo "============================================"
+echo ""
+# Install Oh-My-ZSH
+sudo -H -u $USERNAME sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+sleep 1
+# Pull zsh theme from github and install 
+wget https://raw.githubusercontent.com/cerebralnomad/post_install_script/master/clay.zsh-theme -O ~/.oh-my-zsh/themes/clay.zsh-theme
+sleep 1
+# Install Calibre
+wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sh /dev/stdin 2>> /home/$USERNAME/Documents/post_install_error.log
 
 echo "Additional sources added"
 echo ""
-
-echo "Updating Sources"
-echo "================"
-apt update
-clear
-
+sleep 2
 echo "Installing Applications"
 echo "======================="
 echo ""
 for app in "${apps[@]}" ; do
 	if apt install -y $app ; then 
 		printf "\n$app installed... \n\n"
+        sleep 1
 	else 
 		printf "\n$app install FAILED! \n\n" 2>> /home/$USERNAME/Documents/post_install_error.log
+        sleep 1
 	fi
 done
 echo ""
-
-if [[ $DISTRO == "y"* || $DISTRO == "Y"* ]] ; then
-	apt install -y latte-dock 2>> /home/$USERNAME/Documents/post_install_error.log && printf "\nLatte Dock installed... \n\n"
-	apt install yakuake -y && printf "\nYAKUAKE installed...\n\n"
-fi
+sleep 1
+echo "Installing Flatpaks"
+echo "==================="
+echo ""
+for app in "${flatpaks[@]}" ; do
+        if flatpak install flathub $app ; then
+                printf "\n$app installed... \n\n"
+                sleep 1
+        else
+                printf "\n$app install FAILED! \n\n" 2>> /home/$USERNAME/Documents/post_install_error.log
+                sleep 1
+        fi
+done
 
 # Install yt-dlp, a better youtube-dl replacement
 
@@ -203,27 +221,30 @@ curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr
 chmod a+rx /usr/local/bin/yt-dlp
 echo "yt-dlp installed"
 echo ""
-
-echo "Installing Terminal Addons for Fresh Install"
-echo "============================================"
-echo ""
-echo "Updating Repositories..."
-echo ""
-apt update
-echo ""
-clear
+sleep 1
 echo "Installing Terminal Addons for Fresh Install"
 echo "============================================"
 echo ""
 
 for pkg in "${addons[@]}" ; do
-	apt install -y $pkg
-	printf "\n$pkg installed... \n\n"
+        if apt install -y $pkg ; then
+                printf "\n$pkg installed... \n\n"
+                sleep 1
+        else
+                printf "\n$pkg install FAILED! \n\n" 2>> /home/$USERNAME/Documents/post_install_error.log
+                sleep 1
+        fi
 done
 
-npm install -g tldr && printf "\nTLDR installed... \n\n"
+# Create symlink for bat in .local directory; bat is already a system alias
+ln -s /usr/bin/batcat ~/.local/bin/bat
 
+# Install the TL/DR utility
+npm install -g tldr && printf "\nTLDR installed... \n\n"
+sleep 1
+# Change the default shell from bash to zsh
 chsh -s /usr/bin/zsh
+
 echo ""
 echo "Terminal Addons and all applications installed... 
 Remember to configure Git without using sudo privileges after this script has exited.
@@ -233,8 +254,7 @@ git config --global user.email me@example.com
 
 Log out and back in or reboot to activate the ZSH shell.
 Replace the ~/.zshrc with your custom backup.
-Place clay.zsh-theme in ~/.oh-my-zsh/themes/
-Add your aliases file to the home directory.
+Add your aliases file to the bin directory and symlink in the home directory.
 
 Setup script finished."
 
