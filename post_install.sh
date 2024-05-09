08ffc692f32748c3e69204c897da6fa242338770
Clay <cerebralnomad@protonmail.com>
2024-05-09 04:36:19 -0400

    add qbittorrent flatpack and AppImage Launcher

--- post_install.sh
+++ post_install.sh
@@ -61,6 +61,7 @@ apps=(
 	bucklespring # utility to make kestrokes mimic IBM keyboard
     nomacs # Image viewer - requires 'Universe' repo to be enabled
     mediainfo-gui # graphical frontend for mediainfo
+    appimagelauncher # integrate AppImages into the system menu
     )
 
 # Snap packages
@@ -77,6 +78,7 @@ flatpaks=(
         fr.handbrake.ghb
         com.cerebralnomad.recipescribe
         org.gnome.Sudoku
+        org.qbittorrent.qBittorrent
         com.sigil_ebook.Sigil
 )
 
@@ -116,8 +118,8 @@ echo "Adding Required Repositories"
 echo "============================"
 echo ""
 
-# Add the qbittorrent repo
-add-apt-repository -y ppa:qbittorrent-team/qbittorrent-stable 2>> /home/$USERNAME/Documents/post_install_error.log && printf '\nqbittorrent repo added...\n\n'
+# Add the AppImage Launcher repo
+add-apt-repository ppa:appimagelauncher-team/stable 2>> /home/$USERNAME/Documents/post_install_error.log && printf '\nAppImage Launcher repo added...\n\n'
 sleep 2
 
 # MKVToolnix repo
