55f72d2699b6d3fdcb721f047f922bcf60dba6d1
Clay <cerebralnomad@protonmail.com>
2024-05-10 04:25:07 -0400

    Incremental change

--- post_install.sh
+++ post_install.sh
@@ -15,6 +15,11 @@ fi
 
 clear
 
+# Set variables for colorizing the terminal output
+red='\033[31m'
+green='\033[32m'
+reset='\033[0m'
+
 # Create the log file for any errors
 
 USERNAME="$SUDO_USER"
