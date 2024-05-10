b846b5b8e7df08c449e5c294e1c382f6b0f61503
Cerebral Nomad <me@example.com>
2024-05-10 10:45:29 -0400

    tweaking for further testing

--- post_install_testing.sh
+++ post_install_testing.sh
@@ -178,7 +178,7 @@ snap remove firefox
 install -d -m 0755 /etc/apt/keyrings  # Create an APT keyring (if one doesn’t already exist)
 # Import the Mozilla APT repo signing key
 wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
-# Add the Mozilla signing key to your sources.list
+https://raw.githubusercontent.com/cerebralnomad/post_install_script/master/post_install_testing.sh# Add the Mozilla signing key to your sources.list
 echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
 # Set the Firefox package priority to ensure Mozilla’s Deb version is always preferred
 echo '
@@ -341,7 +341,11 @@ done
 
 # Create symlink for bat in .local directory
 # bat installs as batcat on Ubuntu due to a name clash with another existing package
-ln -s /usr/bin/batcat /home/$USERNAME/.local/bin/bat
+sudo -H -u $USERNAME mkdir /home/$USERNAME/.local/bin
+echo 'export PATH="/home/$USERNAME/.local/bin:$PATH"' >> /home/$USERNAME/.zshrc
+echo 'export PATH="/home/$USERNAME/bin:$PATH"' >> /home/$USERNAME/.zshrc
+source /home/$USERNAME/.zshrc
+sudo -H -u $USERNAME ln -s /usr/bin/batcat /home/$USERNAME/.local/bin/bat
 
 # Install the TL/DR utility
 if npm install -g tldr ; then
