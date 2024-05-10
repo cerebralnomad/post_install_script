a17350c78971fab9e11de5d10e6df1e299124614
Clay <cerebralnomad@protonmail.com>
2024-05-10 04:25:39 -0400

    adding testing file until bugs fixed

---
+++ post_install_testing.sh
@@ -0,0 +1,352 @@
+#!/bin/bash
+
+# Script to install my regular suite of programs on a fresh OS installation
+# Run with sudo
+# This script is for Ubuntu flavors not running KDE (Kubuntu)
+# It will only be tested with MATE
+# Plasma rules Gnome drools
+
+# Check if the script was run as root
+
+if [[ $EUID -ne 0 ]]; then
+	echo "This script must be run with sudo privileges"
+	exit 1
+fi
+
+clear
+
+# Set variables for colorizing the terminal output
+red='\033[31m'
+green='\033[32m'
+lightgreen='\033[32m'
+reset='\033[0m'
+
+# Create the log file for any errors
+
+USERNAME="$SUDO_USER"
+
+sudo -H -u $USERNAME touch /home/$USERNAME/Documents/post_install_error.log 
+
+echo 'Error log created at:'
+echo "/home/$USERNAME/Documents/post_install_error.log"
+sleep 3
+
+# Increase the bash history size to allow scrolling back through the output
+export HISTSIZE=-1
+export HISTFILESIZE=-1
+source /home/$USERNAME/.bashrc
+
+# Create the variables for the various packages to be installed
+# Comment out any you do not want/need
+
+# Command line based tools and non-interactive libraries
+
+packages=(
+	wget
+	curl
+	python3-pip # PIP package manager for python 3
+	python3-venv # Python 3 virtual environment manager
+	default-jdk # Java developer Kit
+    default-jre # Java Runtime Environment
+	git
+	apt-transport-https # Needed for Syncthing
+	zsh
+	)
+
+# GUI applications 
+
+apps=(
+        alacarte # application menu editor
+        appimagelauncher # integrate AppImages into the system menu - from 3rd party PPA
+        bleachbit # System cleaner
+        brave-browser
+        dconf-editor # configure application settings on GTK distros
+        exiftool  # tool for displaying metadata of epub and other formats
+        gimp
+        gnupg2 # PGP handling
+        gnubg  # Backgammon game
+        gpa # Graphical frontend for gnupg
+        gparted # disk partition manager
+        gtkhash # GUI checksum application
+        keepass2 # Local password vault
+        mediainfo-gui # graphical frontend for mediainfo
+        mp3splt-gtk # graphical frontend for mp3splt
+        nomacs # Image viewer - requires 'Universe' repo to be enabled
+        syncthing # File Syncronization between systems - will install from 3rd party PPA
+        timeshift # system state automatic backup
+        zim # Personal Wiki
+    )
+
+# Snap packages
+
+#snaps=(
+#    newpass
+#	)
+
+# Flatpaks
+
+flatpaks=(
+        com.discordapp.Discord
+        org.flatpak.Builder
+        fr.handbrake.ghb
+        com.cerebralnomad.recipescribe
+        org.gnome.Sudoku
+        org.qbittorrent.qBittorrent
+        org.bunkus.mkvtoolnix-gui
+        com.calibre_ebook.calibre
+        com.sigil_ebook.Sigil
+        org.torproject.torbrowser-launcher
+)
+
+#Terminal addons
+	
+addons=(
+	    bat  # a cat clone with syntax highlighting
+        btop  # Resource monitor, like htop but better
+        bucklespring # utility to make kestrokes mimic IBM keyboard
+        checkinstall # make install replacement for building from source
+        cowsay
+        dmidecode # reports information about your system's hardware as described in your system BIOS
+        dstat  # tool for generating system resource statistics
+        duf # a better du command to display disk usage/free
+        fastfetch # alternative to neofetch which is no longer maintained
+        ffmpeg  # command line video converter
+        figlet # create ASCII art from plain text
+        fortune-mod # display fortunes in the terminal
+        fortunes-off # adds offensive fortunes (fortune -o)
+        gcp  # advanced command-line file copier
+        hollywood # silly program to make the terminal look like 1337 H4X0R
+        hwinfo # display details abou tsystem hardware
+        inxi # command line system information script
+        imagemagick
+        libpwquality-tools  # for the pwscore utility to check pw strength
+        mediainfo # display information about audio/video files
+        mp3gain # mp3 volume adjuster
+        mp3wrap # joins mp3 files
+        mp3splt # split mp3 files without decoding
+        ncdu # NCurses Disk Usage utility
+        nmap # network scanning tool
+        nodejs # JavaScript runtime
+        npm # JavaScript package manager
+        rename # batch rename files using sed syntax
+        simplescreenrecorder
+        terminator # terminal emulator
+        tree # list contents of directories in a tree-like format
+        vim # terminal text editor
+	)
+
+echo -e "${lightgreen}Updating Repos and Upgrading System Files${reset}"
+echo -e "${lightgreen}=========================================${reset}"
+add-apt-repository universe
+add-apt-repository multiverse
+add-apt-repository restricted
+apt update && apt dist-upgrade -y
+sleep 2
+
+# Install the packages first as wget and curl are needed in the next steps
+
+for pkg in "${packages[@]}" ; do
+    if apt install -y $pkg ; then
+        printf "\n$pkg installed... \n\n"
+        sleep 1
+    else
+        printf "\n$pkg install FAILED! \n\n" 2>> /home/$USERNAME/Documents/post_install_error.log
+        sleep 1
+    fi
+done
+
+echo -e "${lightgreen}Additional Program Installation for a Fresh OS Install${reset}"
+echo -e "${lightgreen}======================================================${reset}"
+echo ""
+echo -e "${lightgreen}Adding Required Repositories${reset}"
+echo -e "${lightgreen}============================${reset}"
+echo ""
+
+# Add the AppImage Launcher repo
+apt install software-properties-common
+add-apt-repository ppa:appimagelauncher-team/stable 2>> /home/$USERNAME/Documents/post_install_error.log 
+echo ""
+echo -e "${green}AppImage Launcher repo added...${reset}"
+sleep 2
+
+# Syncthing repo
+curl -s https://syncthing.net/release-key.txt | apt-key add - 2>> /home/$USERNAME/Documents/post_install_error.log
+echo 'deb https://apt.syncthing.net/ syncthing stable' | tee /etc/apt/sources.list.d/syncthing.list 2>> /home/$USERNAME/Documents/post_install_error.log 
+echo ""
+echo -e "${green}Syncthing repo installed${reset}"
+sleep 2
+
+# Remove the Firefox snap and setup the Mozilla PPA to install the .deb version (thank you Mozilla) because the snap sucks 
+snap remove firefox
+install -d -m 0755 /etc/apt/keyrings  # Create an APT keyring (if one doesn’t already exist)
+# Import the Mozilla APT repo signing key
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+# Add the Mozilla signing key to your sources.list
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+# Set the Firefox package priority to ensure Mozilla’s Deb version is always preferred
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+
+# Add the Brave browser repo for the once or twice a year when a site doesn't work with Firefox
+curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+echo -e "${green}Finished addding repos...${reset}"
+echo ""
+echo -e "${lightgreen}Updating Newly Added Repos${reset}"
+echo -e "${lightgreen}==========================${reset}"
+apt update
+
+sleep 2
+echo ""
+echo -e "${lightgreen}Installing Additional Tools${reset}"
+echo -e "${lightgreen}===========================${reset}"
+echo ""
+
+# Install Flatpak because Canonical is being a dick about it
+
+echo -e "${lightgreen}Installing Flatpak${reset}"
+echo -e "${lightgreen}==================${reset}"
+echo ""
+sudo apt install flatpak -y
+sleep 1
+echo ""
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+echo ""
+echo -e "${green}Flatpak installed...${reset}"
+echo ""
+sleep 2
+
+echo -e "${lightgreen}Adding Additional Sources with wget and curl${reset}"
+echo -e "${lightgreen}============================================${reset}"
+echo ""
+# Install Oh-My-ZSH
+sudo -H -u $USERNAME sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+sleep 1
+# Pull zsh theme from github and install 
+wget https://raw.githubusercontent.com/cerebralnomad/post_install_script/master/clay.zsh-theme -O /home/$USERNAME/.oh-my-zsh/themes/clay.zsh-theme
+sleep 1
+
+echo -e "${green}Additional sources added${reset}"
+echo ""
+sleep 2
+echo -e "${lightgreen}Installing Applications${reset}"
+echo -e "${lightgreen}=======================${reset}"
+echo ""
+# Install of firefox will occasionally fail if -y is used without --allow-downgrades
+# so it is installed separately
+if apt install -y --allow-downgrades firefox ; then
+        echo ""
+        echo -e "${green}Firefox installed...${reset}"
+        echo ""
+        sleep 1
+else
+        echo""
+        echo -e "${red}Firefox install FAILED!${reset}" 2>> /home/$USERNAME/Documents/post_install_error.log
+        echo ""
+        sleep 1
+fi
+
+
+for app in "${apps[@]}" ; do
+	if apt install -y $app ; then
+        echo "" 
+		echo -e "${green}$app installed...${reset}"
+        echo ""
+        sleep 1
+	else 
+		echo ""
+        echo -e "${red}$app install FAILED!${reset}" 2>> /home/$USERNAME/Documents/post_install_error.log
+        echo ""
+        sleep 1
+	fi
+done
+echo ""
+sleep 1
+
+echo -e "${lightgreen}Installing Flatpaks${reset}"
+echo -e "${lightgreen}===================${reset}"
+echo ""
+for app in "${flatpaks[@]}" ; do
+        if flatpak install -y flathub $app ; then
+                echo ""
+                echo -e "${green}$app installed...${reset}"
+                echo ""
+                sleep 1
+        else
+                echo -e "${red}$app install FAILED!${reset}" 2>> /home/$USERNAME/Documents/post_install_error.log
+                sleep 1
+        fi
+done
+
+# Install yt-dlp, a better youtube-dl replacement
+echo ""
+echo -e "${lightgreen}Installing yt-dlp${reset}"
+echo ""
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+chmod a+rx /usr/local/bin/yt-dlp
+
+# Add the configuration file to default to best video and audio in mp4 format
+# Other things such as default save location can be added to this file later as desired
+# Comment the next 2 lines to skip the creation of the config file
+echo "# Always choose the best available quality" > /etc/yt-dlp.conf
+echo '-f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"' >> /etc/yt-dlp.conf
+
+echo -e "${green}yt-dlp installed...${reset}"
+echo ""
+sleep 1
+
+echo -e "${lightgreen}Installing Terminal Addons for Fresh Install${reset}"
+echo -e "${lightgreen}============================================${reset}"
+echo ""
+
+for pkg in "${addons[@]}" ; do
+        if apt install -y $pkg ; then
+                echo ""
+                echo -e "${green}$pkg installed...${reset}"
+                echo ""
+                sleep 1
+        else
+                echo ""
+                echo -e "${red}$pkg install FAILED!${reset}" 2>> /home/$USERNAME/Documents/post_install_error.log
+                echo ""
+                sleep 1
+        fi
+done
+
+# Create symlink for bat in .local directory
+# bat installs as batcat on Ubuntu due to a name clash with another existing package
+ln -s /usr/bin/batcat /home/$USERNAME/.local/bin/bat
+
+# Install the TL/DR utility
+if npm install -g tldr ; then
+        echo ""
+        echo -e "${green}TL/DR installed...${reset}"
+        echo ""
+else
+        echo ""
+        echo -e "${red}TL/DR install FAILED!${reset}" 2>> /home/$USERNAME/Documents/post_install_error.log
+        echo ""
+fi
+
+sleep 1
+
+# Change the default shell from bash to zsh
+chsh -s /usr/bin/zsh
+
+echo ""
+echo -e "${green}Terminal Addons and all applications installed... 
+Remember to configure Git without using sudo privileges after this script has exited.
+
+git config --global user.name <username>
+git config --global user.email me@example.com
+
+Log out and back in or reboot to activate the ZSH shell.
+Replace the ~/.zshrc with your custom backup.
+Add your aliases file to the bin directory and symlink in the home directory.
+
+Setup script finished.${reset}"
+
